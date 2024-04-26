36987ba3b0967d8d8230c35423139770084d3311
Javier <javier.castilla101@alu.ulpgc.es>
2024-04-27 00:00:13 +0100

    Más cosas hechas

--- misala.c
+++ misala.c
@@ -8,6 +8,7 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <errno.h>
+#include <sys/wait.h>
 
 char SALTO = '\n';
 char BACKSPACE = '\b';
@@ -98,6 +99,7 @@ int reserva(char** argv, int idsNumber) {
     }
 
     int free = 0;
+    off_t lastOffset = lseek(fd, 0, SEEK_CUR);
 
     while (1) {
         size_t size = read(fd, buffer, 1);
@@ -115,20 +117,55 @@ int reserva(char** argv, int idsNumber) {
         exit(-1);
     }
 
-    int flag = 0;
-    int num = 0;
-    
-    int aux = open("aux", O_RDWR | O_CREAT, 0644);
+    free -= idsNumber;
+    int aux = open("aux", O_RDWR | O_CREAT | O_TRUNC, 0644);
 
     if (aux == -1) {
         fprintf(stderr, "%s\n", strerror(errno));
         exit(-1);
     }
-    
+
+    lseek(fd, -1, SEEK_CUR);
+
+    while (1) {
+        size_t size = read(fd, buffer, 10);
+
+        for (int i = 0; i < size; i++) {
+            write(aux, &buffer[i], 1);
+        }
+
+        if (size != 10) break;
+    }
+
+    lseek(fd, lastOffset, SEEK_SET);
+    lseek(aux, 0, SEEK_SET);
+    char libres[20];
+    sprintf(libres, "%d", free);
+    write(fd, libres, strlen(libres));
+
+    while (1) {
+        size_t size = read(aux, buffer, 10);
+
+        for (int i = 0; i < size; i++) {
+            write(fd, &buffer[i], 1);
+        }
+
+        if (size != 10) break;
+    }
+
+    int flag = 0;
+    int num = 0;
 
     for (int argNum = 0; argNum < idsNumber; argNum++) {
-        off_t offset;
+        aux = open("aux", O_RDWR | O_CREAT | O_TRUNC, 0644);
+
+        if (aux == -1) {
+            fprintf(stderr, "%s\n", strerror(errno));
+            exit(-1);
+        }
+
         char* id = argv[argNum];
+        lseek(fd, lastOffset, SEEK_SET);
         
         while (1) {
             size_t size = read(fd, buffer, 1);
@@ -142,7 +179,7 @@ int reserva(char** argv, int idsNumber) {
             if (flag && (buffer[0] - '0') == 0) {
                 free--;
 
-                off_t offset = lseek(fd, 0, SEEK_CUR);
+                lastOffset = lseek(fd, 0, SEEK_CUR);
                 
                 while (1) {
                     size_t leidos = read(fd, buffer, 10);
@@ -154,7 +191,7 @@ int reserva(char** argv, int idsNumber) {
                     if (leidos != 10) break;
                 }
                 
-                lseek(fd, offset - 1, SEEK_SET);
+                lseek(fd, lastOffset - 1, SEEK_SET);
                 write(fd, id, strlen(id));
                 flag = 0;
                 break;
@@ -164,6 +201,7 @@ int reserva(char** argv, int idsNumber) {
         }
         
         lseek(aux, 0, SEEK_SET);
+        lastOffset = lseek(fd, 0, SEEK_CUR);
         
         while (1) {
             size_t leidos = read(aux, buffer, 10);
@@ -174,13 +212,102 @@ int reserva(char** argv, int idsNumber) {
             
             if (leidos != 10) break;
         }
+
+        close(aux);
     }
 
     close(fd);
+
+    if (fork() == 0) {
+        execlp("rm", "rm", "aux");
+    } else {
+        wait(NULL);
+    }
+
 }
 
-int anula() {
+int anula(char** argv, int idsNumber) {
+    char buffer[10];
+    char* route = argv[1];
+    argv += 2;
+    
+    int capacity = 0;
 
+    int fd = open(route, O_RDWR, 0644);
+
+    if (fd == -1) {
+        fprintf(stderr, "%s\n", strerror(errno));
+        exit(-1);
+    }
+
+    while (1) {
+        size_t size = read(fd, buffer, 1);
+        if (buffer[0] == SALTO) {
+            break;
+        } else {
+            capacity = capacity * 10 + (buffer[0] - '0');
+        }
+
+        if (size == 0) break;
+    }
+
+    int free = 0;
+    off_t lastOffset = lseek(fd, 0, SEEK_CUR);
+
+    while (1) {
+        size_t size = read(fd, buffer, 1);
+        if (buffer[0] == SALTO) {
+            break;
+        } else {
+            free = free * 10 + (buffer[0] - '0');
+        }
+
+        if (size == 0) break;
+    }
+
+    if (free == capacity) {
+        exit(0);
+    }
+
+    int flag = 0;
+    int num = 0;
+
+    for (int argNum = 0; argNum < idsNumber; argNum++) {
+        int id = atoi(argv[argNum]);
+        int asiento = 0;
+        
+        while (1) {
+            size_t size = read(fd, buffer, 1);
+
+            if (size == 0) break;
+            if (buffer[0] == '\n') continue;
+            if (buffer[0] == ' ') {
+                flag = 1;
+                continue;
+            }
+            if (flag && asiento == id) {
+                free--;
+
+                while (1) {
+                    read(fd, buffer, 2);
+
+                    if (buffer[0] != '\n') break;
+                    printf("%s\n", buffer[0]);
+                    write(fd, (buffer[1] == '\n') ? "0" : &BACKSPACE, 1);
+                    //if (buffer[1] == '\n') break;
+                }
+
+            } else if (flag) {
+                flag = 0;
+                asiento = 0;
+            } else {
+                flag = 0;
+                asiento = asiento * 10 + (buffer[0] - '0');
+            }
+        }
+    }
+
+    close(fd);
 }
 
 int estado() {
@@ -199,7 +326,7 @@ int main(int argc, char** argv) {
     } else if (!strcmp(argv[1], "reserva")) {
         reserva(argv + 2, (argc - 4));
     } else if (!strcmp(argv[1], "anula")) {
-        anula(argv + 2);
+        anula(argv + 2, (argc - 4));
     } else if (!strcmp(argv[1], "estado")) {
         estado(argv + 2);
     } else {
