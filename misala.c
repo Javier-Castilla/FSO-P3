1d11720b28e87b3dc6541adab2728d3955c37cac
Javier <javier.castilla101@alu.ulpgc.es>
2024-04-27 13:56:49 +0100

    Para Juanqui

--- misala.c
+++ misala.c
@@ -12,6 +12,23 @@
 
 char SALTO = '\n';
 char BACKSPACE = '\b';
+char delete = '\0';
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                swap(&arr[j], &arr[j+1]);
+            }
+        }
+    }
+}
 
 int crea(char** argv) {
     char* route = argv[1];
@@ -230,6 +247,14 @@ int anula(char** argv, int idsNumber) {
     char buffer[10];
     char* route = argv[1];
     argv += 2;
+
+    int ids[idsNumber];
+
+    for (int i = 0; i < idsNumber; i++) {
+        ids[i] = atoi(argv[i]);
+    }
+
+    bubbleSort(ids, idsNumber);
     
     int capacity = 0;
 
@@ -242,28 +267,37 @@ int anula(char** argv, int idsNumber) {
 
     while (1) {
         size_t size = read(fd, buffer, 1);
+
+        if (size == 0) {
+            break;
+        }
+
         if (buffer[0] == SALTO) {
             break;
         } else {
             capacity = capacity * 10 + (buffer[0] - '0');
         }
-
-        if (size == 0) break;
     }
 
     int free = 0;
-    off_t lastOffset = lseek(fd, 0, SEEK_CUR);
+    off_t freeOffset = lseek(fd, 0, SEEK_CUR);
+    off_t lastOffset = 0;
 
     while (1) {
         size_t size = read(fd, buffer, 1);
+
+        if (size == 0) {
+            break;
+        }
+
         if (buffer[0] == SALTO) {
             break;
         } else {
             free = free * 10 + (buffer[0] - '0');
         }
-
-        if (size == 0) break;
     }
+
+    off_t nextFReeOffset = lseek(fd, 0, SEEK_CUR);
 
     if (free == capacity) {
         exit(0);
@@ -272,39 +306,114 @@ int anula(char** argv, int idsNumber) {
     int flag = 0;
     int num = 0;
 
+    off_t lastSeat = lseek(fd, 0, SEEK_CUR);
+
     for (int argNum = 0; argNum < idsNumber; argNum++) {
-        int id = atoi(argv[argNum]);
+        int id = ids[argNum];
         int asiento = 0;
         
+        // Busco el asiento
         while (1) {
             size_t size = read(fd, buffer, 1);
 
-            if (size == 0) break;
-            if (buffer[0] == '\n') continue;
-            if (buffer[0] == ' ') {
-                flag = 1;
-                continue;
+            if (size == 0) {
+                break;
             }
-            if (flag && asiento == id) {
-                free--;
 
-                while (1) {
-                    read(fd, buffer, 2);
+            if (buffer[0] == SALTO) continue;
+            if (buffer[0] == ' ') {
+                if (asiento != id) {
+                    asiento = 0;
 
-                    if (buffer[0] != '\n') break;
-                    printf("%s\n", buffer[0]);
-                    write(fd, (buffer[1] == '\n') ? "0" : &BACKSPACE, 1);
-                    //if (buffer[1] == '\n') break;
+                    while(1) {
+                        read(fd, buffer, 1);
+                        if (buffer[0] == SALTO) {
+                            break;
+                        }
+                    }
+
+                    continue;
+                }
+                //Aquí he encontrado el asiento y ahora elimino su ID asignada
+                int aux = open("aux", O_RDWR | O_CREAT | O_TRUNC, 0644);
+
+                if (aux == -1) {
+                    fprintf(stderr, "%s\n", strerror(errno));
+                    exit(-1);
                 }
 
-            } else if (flag) {
-                flag = 0;
+                lastOffset = lseek(fd, 0, SEEK_CUR); // Guardo el puntero de la posición antes de la ID
                 asiento = 0;
-            } else {
-                flag = 0;
+                // Me posiciono al final de la ID y escribo en un fichero auxiliar un cero y el salto de línea que le sigue
+                while(1) {
+                    read(fd, buffer, 1);
+                    if (buffer[0] == SALTO) {
+                        write(aux, "0\n", 2);
+                        break;
+                    }
+                }
+
+                // Copio todo lo que viene después en otro fichero
+                while (1) {
+                    size_t newSize = read(fd, buffer, 10);
+                    write(aux, buffer, newSize);
+                    if (newSize != 10) break;
+                }
+
+                // Trunco eliminando todo lo que viene
+                ftruncate(fd, lastOffset);
+
+                lastSeat = lseek(fd, 0, SEEK_END) + 2; // Pongo el puntero a donde lo había guardado antes de la ID
+                lseek(aux, 0, SEEK_SET); // Pongo el puntero de AUX al principio para rescribir sobre el otro
+
+                // Empiezo a copiar todo lo de AUX
+                while (1) {
+                    size_t newSize = read(aux, buffer, 10);
+                    write(fd, buffer, newSize);
+                    if (newSize != 10) break;
+                }
+
+                // Cierro el fichero auxiliar y rompo el bucle para ir a otro asiento
+                close(aux);
+
+                if (fork() == 0) {
+                    execlp("rm", "rm", "aux");
+                } else {
+                    wait(NULL);
+                }
+
+                break;
+            } else if ((buffer[0] - '0') >= 0 && (buffer[0] - '0') <= 9) {
                 asiento = asiento * 10 + (buffer[0] - '0');
             }
         }
+        lseek(fd, lastSeat, SEEK_SET);
+    }
+
+    lseek(fd, nextFReeOffset - 1, SEEK_SET);
+
+    int aux = open("aux", O_RDWR, 0644);
+
+
+
+    while (1) {
+        size_t size = read(fd, buffer, 10);
+        write(aux, buffer, size);
+
+        if (size != 10) break;
+    }
+
+    char* newFree; 
+    sprintf(newFree, "%d", free);
+
+    lseek(fd, freeOffset, SEEK_SET);
+    write(fd, newFree, strlen(newFree));
+
+    lseek(aux, 0, SEEK_SET);
+    while (1) {
+        size_t size = read(aux, buffer, 10);
+        write(fd, buffer, size);
+        if (size != 10) break;
     }
 
     close(fd);
